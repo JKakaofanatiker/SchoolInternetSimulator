e8f9790706a5c12234196206e4d5e8b09fc3b96a
J.Kakaofanatiker <53398711+JKakaofanatiker@users.noreply.github.com>
2022-05-24 21:50:07 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(".pain").hide();
+    $(".suffering").hide();
+    $(".depression").hide();
+
+    $(".pain").slideToggle(10000);
+    $(".suffering").slideToggle(90000);
+    $(".depression").slideToggle(8000);
+});
